fcc8f6fc0d4eb58e5ab03ffcb08e366d03d50a4d
zsleader <zssen@vip.qq.com>
2017-12-28 10:46:26 +0000

    final weibo demo

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -121,4 +121,18 @@ class UsersController extends Controller
         $statuses = $user->statuses()->orderBy('created_at','desc')->paginate(30);
         return view('users.show',compact('user','statuses'));
     }
+
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(30);
+        $title = '关注的人';
+        return view('users.show_follow',compact('users','title'));
+    }
+
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate(30);
+        $title = '粉丝';
+        return view('users.show_follow',compact('users','title'));
+    }
 }
